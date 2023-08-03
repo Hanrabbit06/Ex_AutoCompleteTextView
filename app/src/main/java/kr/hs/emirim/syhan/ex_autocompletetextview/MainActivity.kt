0fd9c8f9f0f169818a2c985da48b2326a64dae43
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-08-03 19:05:21 +0900

    Auto, MulticompleteTextView 완성

--- app/src/main/java/kr/hs/emirim/syhan/ex_autocompletetextview/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/syhan/ex_autocompletetextview/MainActivity.kt
@@ -2,12 +2,25 @@ package kr.hs.emirim.syhan.ex_autocompletetextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.MultiAutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var items = arrayOf("소설-전독시","소설-데못죽","소설-화산귀환","Friends","Fringe","Lost","Lost forast")
+        var items = arrayOf("novel-전독시","novel-데못죽","novel-화산귀환","Friends","Fringe","Lost","Lost forest")
+
+        var autoText = findViewById<AutoCompleteTextView>(R.id.autoText)
+        var multiText = findViewById<MultiAutoCompleteTextView>(R.id.multiautoText)
+
+        var adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
+        autoText.setAdapter(adapter)
+
+        var token = MultiAutoCompleteTextView.CommaTokenizer()
+        multiText.setTokenizer(token)
+        multiText.setAdapter(adapter)
 
     }
 }
